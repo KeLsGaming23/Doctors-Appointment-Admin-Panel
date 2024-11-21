80339a147e1b9b34cab695508046eb8fa5946cb5
KelsGaming <alcorizamichael@gmail.com>
2024-11-21 15:40:27 +0800

    Create Add Doctors Function and Get all the Doctors Data from api
    8:10:14

--- src/pages/Admin/DoctorsList.jsx
+++ src/pages/Admin/DoctorsList.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { AdminContext } from '../../context/AdminContext'
 
-export const DoctorsList = () => {
+const DoctorsList = () => {
+
+  const {doctors, aToken, getAllDoctors} = useContext(AdminContext)
+  useEffect(()=>{
+    if (aToken) {
+      getAllDoctors()
+    }
+  },[aToken])
   return (
     <div>DoctorsList</div>
   )
 }
+
+export default DoctorsList
